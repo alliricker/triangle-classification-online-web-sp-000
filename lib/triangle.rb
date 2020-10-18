5f5fd34a213c57a1471e7723c78352ee1d36337f
Allison Ricker <alliricker@gmail.com>
2020-10-18 21:35:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   attr_accessor :length_1, :length_2, :length_3
-
+  
   def initialize(length_1, length_2, length_3)
     @length_1 = length_1
     @length_2 = length_2
@@ -8,7 +8,9 @@ class Triangle
   end
 
   def kind
-    if length_1 == length_2 && length_2 == length_3
+   if length_1 < 0 || length_2 < 0 || length_3 < 0
+     raise TriangleError
+   elsif length_1 == length_2 && length_2 == length_3
       :equilateral
     elsif length_1 != length_2 && length_2 != length_3 && length_1 != length_3
       :scalene
